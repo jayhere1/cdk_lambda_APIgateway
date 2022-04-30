5b3124cf0607854f8c93dc7b563975ed2a6eae48
Jay <jay.veer1@protonmail.com>
2022-04-30 13:43:23 +0100

    Changed folder name

--- tests/unit/test_cdk_projects_stack.py
+++ tests/unit/test_cdk_projects_stack.py
@@ -1,10 +1,12 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from cdk_projects.cdk_projects_stack import CdkProjectsStack
+from cdk_lambda.cdk_lambda_stack import CdkProjectsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cdk_projects/cdk_projects_stack.py
+
+
 def test_sqs_queue_created():
     app = core.App()
     stack = CdkProjectsStack(app, "cdk-projects")
